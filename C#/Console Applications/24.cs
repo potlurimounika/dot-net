26c5ebc770fb8295ffaea1dacc35492206f8665b
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 22:37:05 +0530

    Create 24.cs

---
+++ C#/Console Applications/24.cs
@@ -0,0 +1,14 @@
+using System;
+int a, b;
+System.Console.Write("Enter a:");
+a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter b:");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("a is big");
+}
+else
+{
+    Console.WriteLine("b is big");
+}
