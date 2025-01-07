3ce72c3607ec71c3315e6f5d070bf57f42fa549c
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 11:54:35 +0530

    Create 09.cs

---
+++ C#/Console Applications/09.cs
@@ -0,0 +1,22 @@
+string bcode = "B101";
+int bno = 1;
+string bname = "UNIX";
+float bprice = 456.75f;
+char bpcode = 'U';
+bool bav = true;
+System.Console.WriteLine("........................");
+System.Console.WriteLine("BOOK DETAILS");
+System.Console.WriteLine("........................");
+System.Console.WriteLine("code="+bcode);
+System.Console.WriteLine("Number=" + bname);
+System.Console.WriteLine("price=" + bprice);
+System.Console.WriteLine("print code=" + bpcode);
+System.Console.WriteLine("availabulity=" + bav);
+System.Console.WriteLine("........................");
+
+
+
+
+
+
+
