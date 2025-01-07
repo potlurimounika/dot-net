e751f03d37f9c615ccfa664c48981cdb4d03ae8f
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 11:59:29 +0530

    Create 15.cs

---
+++ C#/Console Applications/15.cs
@@ -0,0 +1,11 @@
+using System;
+int a, b;
+System.Console.Write("Enter a..:");
+a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter b..:");
+b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("values before swap a=" + a + "\t" + "b=" + b);
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+System.Console.WriteLine("values after swap a=" + a + "\t" + "b=" + b);
