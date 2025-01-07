c3cf672015d2a240c75020b2d0d331f48c87ec1a
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 22:28:58 +0530

    Create 18.cs

---
+++ C#/Console Applications/18.cs
@@ -0,0 +1,8 @@
+using System;
+int l, b, a;
+System.Console.Write("Enter length");
+l = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter breadth");
+b = Convert.ToInt32(Console.ReadLine());
+a = l * b;
+System.Console.WriteLine("Area of length:" + a);
