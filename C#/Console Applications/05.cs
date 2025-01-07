efdd5f76bfca8c0e3d23a4f4d0d03c3c3aad8b09
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 11:47:36 +0530

    Create 05.cs

---
+++ C#/Console Applications/05.cs
@@ -0,0 +1,6 @@
+int a = 5, b = 3,c;
+System.Console.WriteLine("values before swap a=" + a + "\t" + "b=" +b);
+c = a;
+a = b;
+b = c;
+System.Console.WriteLine("values after swap a=" + a + "\t" + "b="  +b);
