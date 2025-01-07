73c068df9599b4fbe337733ecaf0a6e23b8eaf44
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 11:55:26 +0530

    Create 10.cs

---
+++ C#/Console Applications/10.cs
@@ -0,0 +1,6 @@
+int a = 5, b = 3;
+System.Console.WriteLine("Addition"+(a+b));
+System.Console.WriteLine("Subtraction" + (a - b));
+System.Console.WriteLine("Multiplication" + (a * b));
+System.Console.WriteLine("Division" + (a / b));
+System.Console.WriteLine("Module" + (a % b));
