1b0bf0b2b8bd078bbffc04761b9f39e285a983de
potlurimounika <mounikapotluri153@gmail.com>
2025-01-07 22:33:38 +0530

    Create 21.cs

---
+++ C#/Console Applications/21.cs
@@ -0,0 +1,8 @@
+using System;
+int b,h,a;
+System.Console.Write("Enter b:");
+b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter h:");
+h = Convert.ToInt32(Console.ReadLine());
+a = (b * h) /2;
+System.Console.Write("Area of tringle=" + a);
