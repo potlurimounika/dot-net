064c4e772f739e3a9e85fabb7f66f53b32273294
potlurimounika <mounikapotluri153@gmail.com>
2025-01-08 10:05:31 +0530

    Create 32.cs

---
+++ C#/Console Applications/32.cs
@@ -0,0 +1,13 @@
+using System;
+int  a,x;
+System.Console.Write("Enter a:");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+    System.Console.WriteLine("Even");
+}
+else
+{
+    System.Console.WriteLine("odd");
+}
