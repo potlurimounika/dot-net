342650f2f7e5e936b355c91b779852754e68387f
potlurimounika <mounikapotluri153@gmail.com>
2025-01-15 20:36:55 +0530

    Update 48.cs

--- C#/Console Applications/48.cs
+++ C#/Console Applications/48.cs
@@ -1,1 +1,19 @@
-
+using System;
+int i = 1, s = 0, n, t, r;
+System.Console.Write("Enter a number:");
+n = Convert.ToInt32(Console.ReadLine());
+t = n;
+while (i <= n)
+{
+    r = n % 10;
+    s = s * 10 + r;
+    n = n / 10;
+}
+if (t == s)
+{
+    System.Console.WriteLine("Palindrome");
+}
+else
+{
+    System.Console.WriteLine("Not");
+}
