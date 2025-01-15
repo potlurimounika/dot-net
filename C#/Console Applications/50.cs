c887b1ce600c6c779707e4085864804b915ef4e4
potlurimounika <mounikapotluri153@gmail.com>
2025-01-15 20:46:16 +0530

    Create 50.cs

---
+++ C#/Console Applications/50.cs
@@ -0,0 +1,13 @@
+using System;
+int i = 1;
+while (i <= 5)
+{
+    int j = 1;
+    while (j <= 5)
+    { 
+    System.Console.WriteLine("*");
+        j = j + 1;
+}
+    i = i + 1;
+    System.Console.WriteLine(" ");
+}
