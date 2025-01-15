cbb90b55014e307f5c382792622642934c553057
potlurimounika <mounikapotluri153@gmail.com>
2025-01-15 19:22:06 +0530

    Create 39.cs

---
+++ C#/Console Applications/39.cs
@@ -0,0 +1,16 @@
+using System;
+int i = 1;
+while (i <= 10)
+{
+    System.Console.WriteLine(i);
+    if(i%2==0)
+    {
+        System.Console.WriteLine("vijayawada");
+    }
+    else
+    {
+        System.Console.WriteLine("nipuna");
+
+    }
+    i = i + 1;
+}
