054bc73d6ddd6787432ab0db89d28052d57bb666
potlurimounika <mounikapotluri153@gmail.com>
2025-01-20 10:51:59 +0530

    Create 63.cs

---
+++ C#/Console Applications/63.cs
@@ -0,0 +1,8 @@
+using System;
+int[] a = new int[5];
+a[0] = 10;
+a[1] = 20;
+a[2] = 30;
+a[3] = 40;
+a[4] = 50;
+System.Console.WriteLine(a[0] + "\t" + a[1]+"\t"+ a[2] + "\t" + a[3] + "\t" + a[4]);
