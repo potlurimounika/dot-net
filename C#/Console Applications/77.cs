b521f29330e71880f6a86401488a8c5dda5795f8
potlurimounika <mounikapotluri153@gmail.com>
2025-02-07 10:01:52 +0530

    Create 77.cs

---
+++ C#/Console Applications/77.cs
@@ -0,0 +1,13 @@
+using System;
+class Bird
+{
+    public void fly()
+        {
+        System.Console.WriteLine("They fly to fulfill natural activities");
+        }
+       public static void Main(string[] args)
+       {
+         Bird parrot = new Bird();
+         parrot.fly();
+       }
+}
