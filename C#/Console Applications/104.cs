d2a53905dcb9297ef8d7e91eaca4b098db6831c8
potlurimounika <mounikapotluri153@gmail.com>
2025-02-08 11:55:46 +0530

    Create 104.cs

---
+++ C#/Console Applications/104.cs
@@ -0,0 +1,14 @@
+using System;
+class Test
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 0;
+        System.Console.WriteLine("Begin");
+        System.Console.WriteLine("Ready to do Division");
+        int c = a / b;
+        Console.WriteLine("Result:" + c);
+        System.Console.WriteLine("End");
+    }
+}
