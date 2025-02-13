c1bcf5352d98d0cda2f7e95a38e8a0cf4a5cf493
potlurimounika <mounikapotluri153@gmail.com>
2025-02-13 11:01:46 +0530

    Create 118.cs

---
+++ C#/Console Applications/118.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+class Test
+{
+ static void Main()
+    {
+        Hashtable hs = new Hashtable();
+        hs.Add("1", "Pen");
+        hs.Add("2", "Pencil");
+        hs.Add("3", "Eraser");
+        ICollection key = hs.Keys;
+        foreach (int k in key)
+        {
+            Console.WriteLine(k + ":" + hs[k]);
+        }
+    }
+}
