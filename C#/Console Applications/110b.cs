4fbd8c5d479af685369dd3e5775c29b35b3a4822
potlurimounika <mounikapotluri153@gmail.com>
2025-02-13 09:39:31 +0530

    Create 110b.cs

---
+++ C#/Console Applications/110b.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HeightWeightInfo
+{
+    class File2
+    {
+    }
+    public partial class Record
+    {
+        public void printRecord()
+        {
+            Console.WriteLine("Height: " + h);
+            Console.WriteLine("Weight: " + w);
+        }
+    }
+}
