c55683f2847c8b39861dae40b03d40467f479419
potlurimounika <mounikapotluri153@gmail.com>
2025-02-13 09:38:55 +0530

    Create 110a.cs

---
+++ C#/Console Applications/110a.cs
@@ -0,0 +1,17 @@
+using System;
+namespace HeightWeightInfo
+{
+    class File1
+    {
+    }
+    public partial class Record
+    {
+        private int h;
+        private int w;
+        public Record(int h, int w)
+        {
+            this.h = h;
+            this.w = w;
+        }
+    }
+}
