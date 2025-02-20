80d0ccb18d4a8c2d84ba749674800fdf67b59791
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:21:22 +0530

    Create 11.vb

---
+++ VB.Net/11.vb
@@ -0,0 +1,15 @@
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 5
+        Dim b As Integer = 3
+        System.Console.WriteLine("Addition=" & (a + b))
+        System.Console.WriteLine("Subtraction=" & (a - b))
+        System.Console.WriteLine("Multiplication=" & (a * b))
+        System.Console.WriteLine("Division=" & (a / b))
+        System.Console.WriteLine("Floor Division=" & (a \ b))
+        System.Console.WriteLine("Modules=" & (a Mod b))
+        System.Console.WriteLine("Exponentation=" & (a ^ b))
+    End Sub
+
+End Module
