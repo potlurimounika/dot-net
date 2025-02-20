ea6852cc3fef9c1883ea400882a47e737ef18239
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:33:01 +0530

    Create 14.vb

---
+++ VB.Net/14.vb
@@ -0,0 +1,10 @@
+Module Module1
+
+    Sub Main()
+        Dim age As Integer
+        System.Console.WriteLine("Enter your age: ")
+        age = Convert.ToInt32(Console.ReadLine())
+        System.Console.WriteLine("your age=" & age)
+    End Sub
+
+End Module
