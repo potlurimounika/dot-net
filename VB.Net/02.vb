05932c6bd15df235a7ac9fdf51087675082e721b
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:07:51 +0530

    Create 02.vb

---
+++ VB.Net/02.vb
@@ -0,0 +1,7 @@
+Module Module1
+    Sub Main()
+        System.Console.WriteLine("welcome to")
+        System.Console.Write("VB.NET")
+        System.Console.Write("vijayawada")
+    End Sub
+End Module
