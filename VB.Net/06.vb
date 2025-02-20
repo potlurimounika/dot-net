71f570df4089a03f8d355feeabaaa74d7496aaae
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:14:13 +0530

    Create 06.vb

---
+++ VB.Net/06.vb
@@ -0,0 +1,10 @@
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 5
+        Dim b As Integer = 3
+        System.Console.WriteLine("value of a=" & a)
+        System.Console.WriteLine("value of b=" & b)
+    End Sub
+
+End Module
