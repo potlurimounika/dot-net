63ca8a95339d11a012419c2a6ea24a6ccc307959
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:22:41 +0530

    Create 12.vb

---
+++ VB.Net/12.vb
@@ -0,0 +1,20 @@
+Module Module1
+
+    Sub Main()
+        Dim ecode As String = "E101"
+        Dim eno As Integer = 101
+        Dim ename As String = "Anil"
+        Dim esal As Double = 10000.0
+        Dim egrade As Char = "A"
+        Dim ews As Boolean = True
+        System.Console.WriteLine("...........................")
+        System.Console.WriteLine("Employee Code : " & ecode)
+        System.Console.WriteLine("Employee No : " & eno)
+        System.Console.WriteLine("Employee Name : " & ename)
+        System.Console.WriteLine("Employee Salary : " & esal)
+        System.Console.WriteLine("Employee Grade : " & egrade)
+        System.Console.WriteLine("Employee Working Status : " & ews)
+        System.Console.WriteLine("...........................")
+    End Sub
+
+End Module
