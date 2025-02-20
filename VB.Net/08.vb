58bb4fc7947716a0385a3a5000483598f6d846f2
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:17:32 +0530

    Create 08.vb

---
+++ VB.Net/08.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 5
+        Dim b As Integer = 3
+        Dim c As Integer
+        System.Console.WriteLine("value of a = " & a & vbTab & "b=" & b)
+        a = a + b
+        b = a - b
+        a = a - b
+        System.Console.WriteLine("value of a = " & a & vbTab & "b=" & b)
+    End Sub
+
+End Module
