fc54662aff302b74865cb297b3f561cc2b7b8b1f
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:10:48 +0530

    Create 04.vb

---
+++ VB.Net/04.vb
@@ -0,0 +1,10 @@
+Module Module1
+    Sub Main()
+        Dim age As Integer = 45
+        System.Console.Title = "Mycode"
+        System.Console.WindowWidth = 300
+        System.Console.WindowHeight = 100
+        System.Console.WriteLine("value of age=" & age)
+    End Sub
+
+End Module
