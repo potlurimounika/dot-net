62c20a4ed1f2396c7dfa96365c5566ff383316f0
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:24:47 +0530

    Create 13.vb

---
+++ VB.Net/13.vb
@@ -0,0 +1,10 @@
+Module Module1
+
+    Sub Main()
+        Dim name As String
+        System.Console.WriteLine("Enter your name: ")
+        name = Console.ReadLine()
+        System.Console.WriteLine("your name=" & name)
+    End Sub
+
+End Module
