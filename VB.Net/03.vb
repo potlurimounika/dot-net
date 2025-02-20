6d8a33bf23895f82d17f5f8c4a9ee200b3632ae4
potlurimounika <mounikapotluri153@gmail.com>
2025-02-20 11:09:18 +0530

    Create 03.vb

---
+++ VB.Net/03.vb
@@ -0,0 +1,9 @@
+Module Module1
+
+    Sub Main()
+        System.Console.WriteLine("Nipuna Technologies")
+        System.Console.WriteLine("vijayawada")
+        System.Console.WriteLine("VB.NET")
+    End Sub
+
+End Module
