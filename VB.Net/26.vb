9ab49ca00f4e676c7593b3eca90c0db9518922f1
potlurimounika <mounikapotluri153@gmail.com>
2025-02-21 11:42:45 +0530

    Create 26.vb

---
+++ VB.Net/26.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim i As Integer = 1
+        Do Until i > 10
+            Console.WriteLine(i)
+            i = i + 1
+        Loop
+        Console.ReadLine()
+    End Sub
+
+End Module
