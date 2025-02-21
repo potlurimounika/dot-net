3d9dac66cbd5014ce9d4155c7a15e8a49cbfb94a
potlurimounika <mounikapotluri153@gmail.com>
2025-02-21 11:53:09 +0530

    Create 29.vb

---
+++ VB.Net/29.vb
@@ -0,0 +1,13 @@
+Module Module1
+
+    Sub Main()
+        Dim a(5) As Integer
+        a(0) = 10
+        a(1) = 20
+        a(2) = 30
+        a(3) = 40
+        a(4) = 50
+        System.Console.WriteLine(a(1))
+    End Sub
+
+End Module
