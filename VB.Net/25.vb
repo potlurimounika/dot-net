34e4b28ea4c5c3b7d177127083e7e7af190ecb63
potlurimounika <mounikapotluri153@gmail.com>
2025-02-21 11:39:54 +0530

    Create 25.vb

---
+++ VB.Net/25.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim i As Integer = 1
+        While i <= 10
+            System.Console.WriteLine(i)
+            i = i + 1
+        End While
+    End Sub
+
+End Module
