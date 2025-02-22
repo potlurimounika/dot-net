a5329380bdaa09eb97f4b3fa955d19bcb1579346
potlurimounika <mounikapotluri153@gmail.com>
2025-02-22 11:19:52 +0530

    Create 32.vb

---
+++ VB.Net/32.vb
@@ -0,0 +1,11 @@
+Class Bird
+    Sub Fly()
+        Console.WriteLine("They fly to fulfill natural activities")
+    End Sub
+End Class
+Module Module1
+    Sub Main()
+        Dim parrot = New Bird()
+        parrot.Fly()
+    End Sub
+End Module
