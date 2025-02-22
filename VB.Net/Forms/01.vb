6a0811bc0951b7736843f915b35de0ffcfbf7b94
potlurimounika <mounikapotluri153@gmail.com>
2025-02-22 14:14:04 +0530

    Create 01.vb

---
+++ VB.Net/Forms/01.vb
@@ -0,0 +1,9 @@
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Red
+        Me.CenterToScreen()
+        Me.Text = "welcome to forms"
+        Me.Width = 400
+        Me.Height = 300
+    End Sub
+End Class
