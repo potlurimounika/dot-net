5d66caf24831e1fc3016a76c97f7eb17c5932808
potlurimounika <mounikapotluri153@gmail.com>
2025-02-27 14:00:08 +0530

    Create 14.vb

---
+++ VB.Net/Forms/14.vb
@@ -0,0 +1,19 @@
+context menu strip:
+
+Public Class Form1
+    Private Sub RedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedToolStripMenuItem.Click
+        Me.BackColor = Color.Red
+    End Sub
+
+    Private Sub BlueToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BlueToolStripMenuItem.Click
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub GreenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GreenToolStripMenuItem.Click
+        Me.BackColor = Color.Green
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
